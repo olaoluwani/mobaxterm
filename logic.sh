04106746ae4fd91585ac781cfd6c187e66e50c93
olaoluwa oduse <olaoluwaoduse@yahoo.com>
2021-10-21 10:40:36 -0400

    files deleted

--- logic.sh
+++
@@ -1,9 +0,0 @@
-# this script returns a logic value
- if [ $1 != '5' ]
- then
-    echo the argument entered is correct 
-    exit 
- else
-   echo the argument entered was not correct
-  exit
-fi
--- simple.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-clear
- echo "thank you jesus" >> /home/user1/bose
-
