a53fdefb2140f7b18067d0eb06df674d1ee5eb0f
olaoluwa oduse <olaoluwaoduse@yahoo.com>
2021-10-21 12:41:50 -0400

    updated code

--- simple.sh
+++ simple.sh
@@ -1,4 +1,34 @@
-#!/bin/bash
-clear
- echo "thank you jesus" >> /home/user1/bose
-
+peline {
+    agent any
+    
+    stages {
+        stage('build') {
+            steps {
+                sh "echo 'i love jesus'"
+                }
+            }
+        stage('test') {
+            steps {
+                sh "echo 'this is my test env'"
+            }
+        }    
+        stage('delivery') {
+            steps {
+                sh "echo 'looking at the outcome'"
+                sh "echo 'keep tracking the event'"
+            }
+        }
+        stage('deployment') {
+            steps {
+                 sh" echo 'thanks for your patronage' "
+            }
+        }
+        stage('deployment2'){
+            steps {
+                sh" echo 'the lord is good' "  
+        }  
+            } 
+    }
+    
+    
+}
