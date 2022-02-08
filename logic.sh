57ba40b0fceef0142c053a65d1bdc1e8e6bcf420
olaoluwani <88695505+olaoluwani@users.noreply.github.com>
2022-02-08 14:03:16 -0500

    Update logic.sh

--- logic.sh
+++ logic.sh
@@ -1,40 +1,4 @@
- pipeline {
-     agent any
-    
-    stages {
-        stage('build') {
-            steps {
-                sh "echo 'i love jesus'"
-                }
-            }
-        stage('test') {
-            steps {
-                sh "echo 'this is my test env'"
-            }
-        }    
-        stage('delivery') {
-            steps {
-                sh "echo 'looking at the outcome'"
-                sh "echo 'keep tracking the event'"
-            }
-        }
-        stage('deployment') {
-            steps {
-                 sh" echo 'thanks for your patronage' "
-            }
-        }
-        stage('deployment2'){
-            steps {
-                sh" echo 'the lord is good' "  
-        }  
-            }
-        stage('deployment3') {
-            steps {
-                sh" echo 'a new trial' "
-        }
-            }    
-    }
-    
-    
-}
-
+ #!/bin/bash
+git add -A
+git commit -m "updated code"
+git push origin master
