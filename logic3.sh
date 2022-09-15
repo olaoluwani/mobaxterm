f53e3ab1bec3cafc2a23e371c99ecdb3306caeab
olaoluwani <88695505+olaoluwani@users.noreply.github.com>
2022-09-15 09:25:55 -0400

    Create logic3.sh

---
+++ logic3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this script returns a logic value
+ if [ $1 != '5' ]
+ then
+    echo the argument entered is correct
+    exit
+ else
+   echo the argument entered was not correct
+  exit
+fi
